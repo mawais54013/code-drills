b3c95ae9539e6f6f51d63692f2a340d417d3ffd1
MAwais <mawais54013@outlook.com>
2018-08-04 19:24:40 -0700

    update

--- week-02/day-04/03/challenge-prompt.js
+++ week-02/day-04/03/challenge-prompt.js
@@ -3,36 +3,37 @@ $(document).ready(function () {
   // Section 1: DEFINING VARIABLES
 
   // 1.1 create a variable called letters with an array of every letter in alphabet (A-Z) as a string
-
+  var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
   // 1.2 create a variable called body and have it equals the jquery selector of body
-
+  var body = $(":body");
 
   // 1.3 create a variable called btns which equals the variable body jQuery called "find" method and in find we are looking fro the id of buttons
-
+  var btns = $("body").find("#hardCoded");
 
   // Section 2 : DYNAMICALLY the text on the button
   // =================================================================================
 
   // 2.1 Create a for-loop that iterates through the variable letters.
-
+    for(var i = 0; i<letters.length; i++)
+    {
 
     // 2.2 Create a variable named "textButton" equal to a jquery selctor of the <button> tag
-
+        var textButton = $("<button>");
 
     // 2.3. Add the class "letter-button" to the variable textButton we just created
-
+        $("textButton").addClass("letter-button");
 
     // 2.4. Then give the variable text button a data-attribute of "data-letter" set to each element within our letters array in turn 
-
+        $("textButton").data("data-letter");
 
     // 2.5. Set the text of each button to match each element in the array
 
 
     // 2.6. Append "textButton" to the variable buttons.
+      $("textButton").append("#buttons");
 
-
-  
+    }
 
   // Section 3 : attaching the button text to a div
   // =================================================================================
