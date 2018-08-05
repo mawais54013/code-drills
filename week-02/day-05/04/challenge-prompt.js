f47db0cbd58a626b733addffa6e6a72f289e1408
MAwais <mawais54013@outlook.com>
2018-08-04 18:30:50 -0700

    update 5

--- week-02/day-05/04/challenge-prompt.js
+++ week-02/day-05/04/challenge-prompt.js
@@ -1,19 +1,16 @@
 console.log("==================== Question 01 ====================");
 
 // make a variable named `word` and set it to an empty string
-
+    var word = "";
 
 // append the string "hello" to `word`
 
-
 // append a space to `word`
-
 
 // append the string "world" to `word`
 
-
 // console.log the `word` variable
-
+console.log(word)
 
 
 console.log("==================== Question 02 ====================");
@@ -29,7 +26,8 @@ var animal = "the fox says";
 // use the `indexOf` method to console log the index of the letter "x" 
 // in the `animal` variable. Do not hard code in the number you want to console
 // log.
-
+var n = animal.indexOf("x");
+console.log(n)
 
 
 console.log("==================== Question 04 ====================");
@@ -38,17 +36,31 @@ animal = "the giraffe says sup";
 
 // using `indexOf`, console log `true` if the string "says" is in the `animal` 
 // variable and `false` if it's not
-
+if(animal.indexOf("says"))
+{
+    console.log(true);
+}
+else 
+{
+    console.log(false);
+}
 
 // using `indexOf`, console.log `true` if the string "zebra" is in the `animal` 
 // variable and `false` if it's not.
 
-
+if(animal.indexOf("zebra"))
+{
+    console.log(true);
+}
+else 
+{
+    console.log(false);
+}
 
 console.log("==================== Question 05 ====================");
 
 // what is the index of the first character of a string?
-//
+//0
 
 
 console.log("==================== Question 06 ====================");
@@ -56,7 +68,7 @@ console.log("==================== Question 06 ====================");
 var word = "fiesta";
 
 // console log the "e" in `word` without explicitly using the string "e"
-
+console.log(word[2]);
 
 
 console.log("==================== Question 07 ====================");
@@ -65,7 +77,7 @@ var greeting = "apple banana chicken";
 greeting = greeting.replace(/a/g, 'g');
 
 // what is the value of the variable greeting?
-
+//gpple bgngng chicken
 
 
 console.log("==================== Question 08 ====================");
@@ -73,7 +85,7 @@ console.log("==================== Question 08 ====================");
 word = "apple";
 
 // console.log "ppl" from `word` without using the letters "ppl"
-
+console.log(word[1],word[2],word[3])
 // HINT: You are looking for a slice of this string, not the whole pie
 
 
@@ -81,10 +93,10 @@ word = "apple";
 console.log("==================== Question 09 ====================");
 
 word = "the giraffe said hi";
-
+word = word[0].toUpperCase();
 // Capitalize the first letter in the `word` variable 
 // console log your modified `word` variable
-
+console.log(word);
 
 
 console.log("==================== Question 10 ====================");
@@ -92,25 +104,26 @@ console.log("==================== Question 10 ====================");
 animal = "moose"; 
 
 //write code here to change the center o to z
-
+var set = animal.replace(animal[2],"z");
+console.log(set);
 
 
 console.log("==================== Question 11 ====================");
 
 // make a variable named `dogs` and initialize it to an empty array
 
+    var dogs = [];
+ // append the string "rover" into the `dogs` array
 
-// append the string "rover" into the `dogs` array
-
-
+    dogs.push("rover");
 // append the string "hunter" into the `dogs` array
 
-
+dogs.push("hunter");
 // append the string "oak" into the `dogs` array
 
-
+dogs.push("oak");
 // console.log out the length of the `dogs` array
-
+console.log(dogs.length)
 
 
 console.log("==================== Question 12 ====================");
@@ -118,10 +131,11 @@ console.log("==================== Question 12 ====================");
 var letters = ['h', 'c', 'i', 'r'];
 
 // reverse the order of the `letters` array
-
+letters.reverse();
 
 // convert the `letters` array into a string and console log it
-
+letters = letters.toString();
+console.log(letters);
 
 
 console.log("==================== Question 13 ====================");
@@ -130,30 +144,38 @@ var famousToms = ["Tom Cruise", "Tom Hanks", "Tom Keel"];
 
 // write code below to replace "Tom Hanks" with "Tom Petty" 
 // in the `famousToms` variable
-
+famousToms[1] = "Tom Petty";
 
 // declare a variable named `nums` and set it to an empty array
-
+var nums = [];
 
 // on the next line write code to add numbers 1 to 1000 into the nums array
-
+for(var i = 1; i < 1000; i++)
+{
+    nums = i + nums[i];
+}
 
 
 console.log("==================== Question 14 ====================");
 
 // write a function named `doubleLength` that takes in a string and returns
 // the length of that string multiplied by 2
-
-
+var string1 = "";
+function doubleLength(string1)
+{
+    string1 = string1.length * 2;
+    return string1;
+}
 // call the `doubleLength` function with a value of "allen" and console log 
 // the return value
-
+doubleLength("allen")
+console.log(doubleLength(string1));
 
 console.log("==================== Question 15 ====================");
 
 // make a variable named `nums` and set it to be an array with the 
 // numbers 1002 and 1001 in it.
-
+var nums = [1002,1001];
 
 // on the next line write code to push the number from 1000 down to 1 
 // into the nums array. The final array should have the numbers from 1002
@@ -179,11 +201,18 @@ console.log("==================== Question 17 ====================");
 
 // write a function named `arrSum` that takes in an array and returns 
 // the sum of all the elements of the array
-
+function arrSum(myArray)
+{
+    for(var i = 0; i<myArray.length; i++)
+    {
+        myArray += myArray[i];
+        return myArray;
+    }
+}
 
 
 myArray = [3,65,24,12,7,81,23,39];
-
+arrSum(myArray);
 // Test your function with `myArray` as the argument
 
 
@@ -220,7 +249,7 @@ function returnSum1(a,b){ return a + b; }
 var part1 = returnSum1();
 
 // What is the value of `part1`?
-//
+// 11
 
 
 
@@ -232,7 +261,7 @@ function returnSum2(){ var a = 9; return a + b; }
 var part2 = returnSum2();
 
 // What is the value of `part2`?
-//
+//9
 
 
 
@@ -244,7 +273,7 @@ function returnSum3(){ return a + b; var a = 9; }
 var part3 = returnSum3();
 
 // What is the value of `part3`?
-//
+//0
 
 
 
@@ -256,7 +285,7 @@ function returnSum4(){ var a = "hi"; return a + b; }
 var part4 = returnSum4();
 
 // What is the value of `part4`?
-//
+//hi
 
 
 
@@ -264,5 +293,6 @@ var num = 0;
 for (var i=9; i>=0; i--){ num += i; }
 
 // What is the value of `num`?
-//
+//9 , 17, 24,30,35,39,42,44,45
+
 
