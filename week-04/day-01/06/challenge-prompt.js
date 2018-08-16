8df44f6ce06f7e0796792f747fe2cf06fac12301
MAwais <mawais54013@outlook.com>
2018-08-15 21:46:57 -0700

    modified

--- week-04/day-01/06/challenge-prompt.js
+++ week-04/day-01/06/challenge-prompt.js
@@ -7,11 +7,27 @@ var database = firebase.database();
 // Document.ready shorthand
 $(function() {
 
+  var text = "";
   // -------------------- Your Code Here --------------------
-
-
+  $("#chat-submit").on("click", function(event) {
+    event.preventDefault();
   
+    text = $("#chat-input").val().trim();
+    
+    $("#chat-input").val(" ");
+    database.ref().push({
+      text: text,
+    });
+ 
+  });
   
+  database.ref().on("child_added", function(childSnapshot)
+  {
+    var chatItem = childSnapshot.val().text;
+
+    $("#chat-display").append(chatItem);
+    $("#chat-display").append("<br>");
+  });
  
 
   // --------------------- End Code Area --------------------
